4b7e71ae4d542816173c4eaf2765715fe1502d5e
Amrit Raj <amrit.raj1504@gmail.com>
2024-07-08 16:59:12 +0530

    bug fixed

--- src/routes/bounty/+page.server.ts
+++ src/routes/bounty/+page.server.ts
@@ -5,30 +5,31 @@ import { NeucronSigner } from "neucron-signer";
 const provider = new DefaultProvider({ network: bsv.Networks.mainnet });
 const signer = new NeucronSigner(provider);
 await signer.login("sales@timechainlabs.io", "string");
-await Root.loadArtifact();
 let instance: any;
 
 export const actions = {
   deploy: async ({ request }) => {
     const data = await request.formData();
-
+    
     const square = BigInt(Number(data.get("square")));
+    await Root.loadArtifact();
     instance = new Root(square);
     await instance.connect(signer);
-
+    
     const deployTx = await instance.deploy(Number(data.get("bounty")));
     console.log(
       "smart lock deployed : https://whatsonchain.com/tx/" + deployTx.id
     );
-
+    
     return { deployed: true, txid: deployTx.id };
   },
-
+  
   unlock: async ({ request }) => {
     // Retrieve data from the form
     const data = await request.formData();
     const root = Number(data.get("root"));
-
+    
+    await Root.loadArtifact();
     await instance.connect(signer);
     // Call the unlock method
     try {
