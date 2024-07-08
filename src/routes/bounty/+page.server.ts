c101cf15fe60d24a806e684f8a191df68feff70c
Amrit Raj <amrit.raj1504@gmail.com>
2024-07-08 17:35:12 +0530

    artifact changed

--- src/routes/bounty/+page.server.ts
+++ src/routes/bounty/+page.server.ts
@@ -1,10 +1,12 @@
 import { Root } from "../../contracts/root";
 import { DefaultProvider, sha256, bsv, toByteString } from "scrypt-ts";
 import { NeucronSigner } from "neucron-signer";
+import artifact from "../../../artifacts/root.json"
 
 const provider = new DefaultProvider({ network: bsv.Networks.mainnet });
 const signer = new NeucronSigner(provider);
 await signer.login("sales@timechainlabs.io", "string");
+await Root.loadArtifact(artifact);
 let instance: any;
 
 export const actions = {
@@ -12,7 +14,6 @@ export const actions = {
     const data = await request.formData();
 
     const square = BigInt(Number(data.get("square")));
-    await Root.loadArtifact();
     instance = new Root(square);
     await instance.connect(signer);
 
@@ -33,7 +34,6 @@ export const actions = {
     const data = await request.formData();
     const root = Number(data.get("root"));
 
-    await Root.loadArtifact("../");
     await instance.connect(signer);
     // Call the unlock method
     try {
